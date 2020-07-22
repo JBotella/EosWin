de21ac2bcc3505f70cf542670998c5d0f08f666b
Juan <juan.botella@microarea.es>
2020-07-22 16:18:18 +0200

    Actualiza css

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -4,7 +4,10 @@
 		$extrabar = 'visible';
 	@endphp
 	<div class="contenedorSeccion @if(isset($extrabar) and $extrabar == 'visible') cSeccExtraVisible @endif">
-		<div class="cabeceraSeccion"><i class="fas fa-tachometer-alt mr-2" onclick="extraBar()"></i>Dashboard</div>
+		<div class="cabeceraSeccion">
+			<span><i class="fas fa-tachometer-alt mr-2" onclick="extraBar()"></i></span>
+			<span>@lang('texto.dashboard')</span>
+		</div>
 		<div class="contenidoSeccion">
 			@include('includes.ejemplos.formulario')
 			@include('includes.ejemplos.tabla')
